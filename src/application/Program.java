39ae9727a55be023803482a941e0ce21dcb3cd90
Charles Sandro <csandro1973@gmail.com>
2022-02-19 10:28:37 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,6 +19,7 @@ public class Program {
 		System.out.println("*** Test 1: seller findByid ****");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		System.out.println();
 		
 		System.out.println("*** Test 2: seller findByDepartment ****");
 		Department department = new Department(2,null);
@@ -26,17 +27,30 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+		System.out.println();
 		System.out.println("*** Test 3: seller findAll ****");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+		System.out.println();
 		System.out.println("*** Test 4: seller insert ****");
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = "+ newSeller.getId());
+		System.out.println();
+		
+		System.out.println("*** Test 5: seller update ****");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println();
+		
+		System.out.println("Lista atualizada");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
